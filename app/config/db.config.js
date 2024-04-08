71cedbdab7c8e3dbd54e0e6bab7ff3ad0e98fee7
Harish Sutar <harishsutar407@gmail.com>
2024-04-08 11:13:12 -0700

    updated pass

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "HArish2403@@",
+    PASSWORD: "<your db password>",
     DB: "demodb",
     dialect: "mysql",
     pool: {
